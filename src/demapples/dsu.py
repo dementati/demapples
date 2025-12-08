fad9fc0499e527bd0104e29619ff83aae71f9475
Lucas Alenbro Lindström <lucas.lindstrom@vermiculus.se>
2025-12-08 13:44:56 +0100

    Enhance DisjointSetUnion class: update docstring examples and ensure size tracking in union method

--- src/demapples/dsu.py
+++ src/demapples/dsu.py
@@ -1,19 +1,24 @@
 class DisjointSetUnion:
     """
-    >>> DisjointSetUnion(5).find(0)
-    0
     >>> dsu = DisjointSetUnion(5)
+    >>> dsu.find(0)
+    0
+    >>> dsu.get_size(0)
+    1
     >>> dsu.union(0, 1)
     >>> dsu.find(1)
     0
     >>> dsu.union(1, 2)
     >>> dsu.find(2)
     0
+    >>> dsu.get_size(0)
+    3
     """
 
     def __init__(self, size: int):
         self.parent = list(range(size))
         self.rank = [1] * size
+        self.size = [1] * size
 
     def find(self, u: int) -> int:
         if self.parent[u] != u:
@@ -27,8 +32,14 @@ class DisjointSetUnion:
         if root_u != root_v:
             if self.rank[root_u] > self.rank[root_v]:
                 self.parent[root_v] = root_u
+                self.size[root_u] += self.size[root_v]
             elif self.rank[root_u] < self.rank[root_v]:
                 self.parent[root_u] = root_v
+                self.size[root_v] += self.size[root_u]
             else:
                 self.parent[root_v] = root_u
+                self.size[root_u] += self.size[root_v]
                 self.rank[root_u] += 1
+
+    def get_size(self, u: int) -> int:
+        return self.size[self.find(u)]
